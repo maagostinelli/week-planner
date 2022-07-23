710cdbb97ba12afcc1ca76dd957a7c2f72595c34
Mariana Agostinelli <marianaagost2@gmail.com>
2022-07-23 18:57:00 -0300

    Styled calendar component

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -5,30 +5,58 @@ import './style.scss'
 export function Calendar() {
     const date = new Date
     const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
-    const lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate()
-    const previousLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate()
 
-    date.setDate(1)
-    const firstDayIndex = date.getDay()
-    const lastDayIndex = new Date(date.getFullYear(), date.getMonth()+1, 0).getDay()
+    function handleRenderCalendar() {    
+        const lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate()
+        const previousLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate()
+    
+        date.setDate(1)
+        const firstDayIndex = date.getDay()
+        const lastDayIndex = new Date(date.getFullYear(), date.getMonth()+1, 0).getDay()
+        
+        let days = []
+        const nextDays = 42 - (firstDayIndex + lastDay)
+    
+        for (let p = firstDayIndex; p >= 1; p--) {
+            days.push(<div className="day prev-days">{previousLastDay - p + 1}</div>)
+        }
+    
+        for (let p = 1; p <= lastDay ; p++) {
+            if (p == new Date().getDate() && date.getMonth() == new Date().getMonth()) {
+                days.push(<div className='day today'>{p}</div>)
+            }
+            else {
+                days.push(<div className='day'>{p}</div>)
+            }
+        }
+    
+        for (let p = 1; p <= nextDays; p++) {
+            days.push(<div className='day next-days'>{p}</div>)
+        }
+        return days
+    }
+
+    function renderPrevMonth() {
+        date.setMonth(date.getMonth()-1)
+    }
 
     return (
         <div className="calendar">
             <div className="month">
-                <CaretLeft size={20} weight="bold" />
+                <CaretLeft size={20} weight="bold" color="#33a9ac"/>
                 <span>{months[date.getMonth()]}</span>
-                <CaretRight size={20} weight="bold" />
+                <CaretRight size={20} weight="bold" color="#33a9ac"/>
             </div>
             <div className="week-days">
-                <div>SEG</div>
-                <div>TER</div>
-                <div>QUA</div>
-                <div>QUI</div>
-                <div>SEX</div>
-                <div>SAB</div>
-                <div>DOM</div>
+                <div>Dom</div>
+                <div>Seg</div>
+                <div>Ter</div>
+                <div>Qua</div>
+                <div>Qui</div>
+                <div>Sex</div>
+                <div>Sáb</div>
             </div>
-            <div className="days"></div>
+            <div className="days">{handleRenderCalendar()}</div>
         </div>
     )
 }
