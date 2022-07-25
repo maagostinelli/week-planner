c20271039eb2d34d88a3fec234eb03fa54191320
Mariana Agostinelli <marianaagost2@gmail.com>
2022-07-24 23:37:34 -0300

    Created TodoItem

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -1,7 +1,27 @@
+interface todoItemProps {
+    id: string,
+    text: string,
+    status: 'active' | 'completed'
+}
+
 export function TodoItem() {
     return(
         <div className="todo-item">
-            
+            <div className="item-status">
+                
+            </div>
+            <p></p>
         </div>
     )
 }
+
+/*
+<div class="todo-item">
+        <div class="check">
+            <div data-id="${item.id}" class="check-mark ${item.status == "complete" ? "checked" : ""}">
+                <img src="./assets/icon-check.svg" alt="">
+            </div>
+        </div>
+    <div class="todo-text ${item.status == "complete" ? "checked" : ""}">${item.text}</div>
+</div>
+ */
